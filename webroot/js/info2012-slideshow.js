eaa0cbb234bf2758bef6afcf5334ca19e54be2cc
Mads Danquah <mads@danquah.dk>
2012-04-22 01:28:16 +0200

    "completed" the layout - should be ready for ticker and clock now, next up: actually fetching json from an external source and reacting on changes

---
+++ webroot/js/info2012-slideshow.js
@@ -0,0 +1,25 @@
+function startSlideshow(){
+
+	// find the content
+	var images = [
+      {
+          "author": "MHD", 
+          "description": "Beskrivelse", 
+          "url": "a.jpeg"
+      }
+  ];
+  
+  
+  for(img in images){
+    alert(img[author]);    
+  }
+}
+
+	// setup the slideshow
+	
+	// start it
+  // $('.content').
+  //     $('.slideshow').cycle({
+  //  fx: 'fade',
+  //  timeout: 1000
+  // });
