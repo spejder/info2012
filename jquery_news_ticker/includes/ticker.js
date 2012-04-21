8765bcf01f580bfa48279e2b6bf084044f886ca5
Johan Thomsen <jot@internetgruppen.dk>
2012-04-21 14:03:42 +0200

    Added jquery_news_ticker

---
+++ jquery_news_ticker/includes/ticker.js
@@ -0,0 +1,13 @@
+
+/**
+ * NewsTicker module for infoscreens SL2012
+ * User: jot
+ * Date: 4/21/12
+ */
+(function() {
+
+    $(function() {
+        $('#js-news').ticker({controls: false, titleText: "Nyheder", displayType: "fade"});
+    });
+
+})();
