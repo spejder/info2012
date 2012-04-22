83f330b7c6f4cd583ec7f4106940aa03cfa2002d
Johan Thomsen <jot@internetgruppen.dk>
2012-04-22 02:11:21 +0200

    Preparing feed_proxy_impl for real life testing tomorrow :)

--- feed_proxy_impl.js
+++ feed_proxy_impl.js
@@ -1,14 +1,14 @@
 var feed_proxy = {
    fetch: function(callback) {
-                $.ajax({
-                            url: feed_source,
-                            dataType: "json",
-                            crossDomain: true, //We might have to set this to true at a later time
-                            success: function(data) {
-                                
-                                if (xhr.statusCode == "200")
-									callback(data);
-                            }
-                        });
+		$.ajax({
+				url: "https://dev.dds.dk/info2012.json",
+				dataType: "json",
+				crossDomain: true,
+				success: function(data, textStatus, xhr) {
+					
+					if (xhr.status == "200")
+						callback(data);
+				}
+			});
    }
 };
