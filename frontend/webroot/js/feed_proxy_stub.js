2eef5e5249b3ddf63e54bd72d8a28226251ccd4b
Mads H. Danquah <mads@danquah.dk>
2012-07-02 22:42:53 +0200

    updated stub

--- frontend/webroot/js/feed_proxy_stub.js
+++ frontend/webroot/js/feed_proxy_stub.js
@@ -3,51 +3,69 @@ var feed_proxy = {
 
 	fetch: function(callback) {
              callback({
-						"images": [
-									      {
-									          "author": "MHD",
-									          "description": "Beskrivelse 1",
-									          "src": "http://flab.dk/~mads/sl2012/info/imgtest/adapted/IMG_0204.jpg",
+							"albums":
+							[
+								{
+									"type": "image"
+									"title": "Billeder fra fredag"
+									"content":
+										[
+											{
+							          "byline": "MHD",
+							          "caption": "Beskrivelse 1",
+							          "src": "http://flab.dk/~mads/sl2012/info/imgtest/adapted/IMG_0204.jpg",
+									      "width": "1144",
+									      "height": "763"
+								      },
+								      {
+							          "byline": "MHD",
+							          "caption": "Beskrivelse 5",
+							          "src": "http://flab.dk/~mads/sl2012/info/imgtest/adapted/IMG_1141.jpg",
+									      "width": "1144",
+									      "height": "763"
+								      }
+										]
+								},
+								{
+									"type": "video"
+									"title": "Alle timelapses"
+									"content":
+										[
+											{
+							          "byline": "MHD",
+							          "caption": "En video",
+							          "src": "pkDKzLUrJ5U",
+								      },
+								      {
+							          "byline": "MHD",
+							          "caption": "En anden video",
+							          "src": "pkDKuLUrJ58",
+								      }
+										]
+								},
+								{
+									"type": "image"
+									"title": "Billeder fra mandag"
+									"content":
+										[
+											{
+							          "byline": "MHD",
+							          "caption": "Beskrivelse 3",
+							          "src": "http://flab.dk/~mads/sl2012/info/imgtest/adapted/IMG_0204.jpg",
 									      "width": "1144",
 									      "height": "763",
-									      },
-									      {
-									          "author": "MHD",
-									          "description": "Beskrivelse 2",
-						  		          "src": "http://flab.dk/~mads/sl2012/info/imgtest/adapted/IMG_0209.jpg",
-									      "width": "547",
-									      "height": "820",
-									      },
-									      {
-									          "author": "MHD",
-									          "description": "Beskrivelse 3",
-									          "src": "http://flab.dk/~mads/sl2012/info/imgtest/adapted/IMG_0323.jpg",
-									      "width": "1144",
-									      "height": "763",
-									      },
-									      {
-									          "author": "MHD",
-									          "description": "Beskrivelse 4",
-									          "src": "http://flab.dk/~mads/sl2012/info/imgtest/adapted/IMG_1073.jpg",
-									      "width": "1144",
-									      "height": "763",
-									      },
-									      {
-									          "author": "MHD",
-									          "description": "Beskrivelse 5",
-									          "src": "http://flab.dk/~mads/sl2012/info/imgtest/adapted/IMG_1141.jpg",
-									      "width": "1144",
-									      "height": "763",
-									      }
-									  ],
-						"news": [
-						"newsstring1",
-						"newsstring2"
-						],
-						"video": [
-						"http://youtube.com/?v=ABCDEF24325"
-						],
-                        "mode": "normal"
+								      },
+								      {
+							          "byline": "MHD",
+							          "caption": "Beskrivelse 4",
+							          "src": "http://flab.dk/~mads/sl2012/info/imgtest/adapted/IMG_1141.jpg",
+							      		"width": "1144",
+								     	 "height": "763",
+								      }
+										]
+								},
+						  ],
+							"news": ["newsstring1", "newsstring2"],
 						});
            }
 }
